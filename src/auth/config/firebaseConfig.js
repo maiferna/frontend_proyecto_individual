18fd91424aa3cb8e9285d76a08abadd6fdecebfa
maiferna <maiderfernandez57@gmail.com>
2025-07-07 20:01:13 +0200

    CHANGE: keys

--- src/auth/config/firebaseConfig.js
+++ src/auth/config/firebaseConfig.js
@@ -7,12 +7,12 @@ import { getFirestore } from 'firebase/firestore';
 
 // Configuración de la web app de Firebase
 const firebaseConfig = {
-  apiKey: `${import.meta.env.API_KEY}`,
-  authDomain: `${import.meta.env.AUTH_DOMAIN}`,
-  projectId: `${import.meta.env.PROJECT_ID}`,
-  storageBucket: `${import.meta.env.STORAGE_BUCKET}`,
-  messagingSenderId: `${import.meta.env.MESSAGING_SENDER_ID}`,
-  appId: `${import.meta.env.APP_ID}`
+  apiKey: import.meta.env.API_KEY,
+  authDomain: import.meta.env.AUTH_DOMAIN,
+  projectId: import.meta.env.PROJECT_ID,
+  storageBucket: import.meta.env.STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.MESSAGING_SENDER_ID,
+  appId: import.meta.env.APP_ID
 };
 
 // Inicializar Firebase con las credenciales del proyecto
